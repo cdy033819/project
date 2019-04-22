ea1337b9f8c202ebbd5da4e6fe7948511b0caff8
cdy03 <462180817@qq.com>
2019-04-22 21:39:33 +0800

    登录功能开发完毕

--- src/components/login.vue
+++ src/components/login.vue
@@ -4,13 +4,84 @@
       <div id="logo-box">
         <img src="../assets/img/logo.png" alt>
       </div>
-      
+      <el-form :rules="loginFormRules" ref="loginFormRef" :model="loginForm">
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username">
+            <i slot="prefix" class="iconfont icon-user"></i>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" show-password>
+            <i slot="prefix" class="iconfont icon-3702mima"></i>
+          </el-input>
+        </el-form-item>
+        <el-row>
+          <el-col :push="15">
+            <el-button type="primary" @click="login()">登录</el-button>
+            <el-button type="info" @click="reset()">重置</el-button>
+          </el-col>
+        </el-row>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      loginFormRules: {
+        //被校验字段名称[{校验规则}，{校验规则}]
+        username: [
+          {
+            required: true,
+            message: '用户名必填',
+            trigger: 'blur'
+          }
+        ],
+        //required:必填项目，message：错误提示，trigger：触发机制
+        password: [
+          {
+            required: true,
+            message: '密码必填',
+            trigger: 'blur',
+          }
+        ]
+      },
+
+      //登录form表单数据对象
+      loginForm: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    reset() {
+      this.$refs.loginFormRef.resetFields()
+    },
+    //管理员登录系统
+    login() {
+      //对各个表单域进行再次验证
+      this.$refs.loginFormRef.validate(async valid => {
+        //valid的表单验证标志信息，true：成功，false：失败
+        if (valid === true) {
+          //用户名的、密码的真实性带着去服务器端完成
+          const { data: dt } = await this.$http.post('/login', this.loginForm)
+          console.log(dt)
+
+          //用户、密码错误提示信息
+          if (dt.meta.status !== 200) {
+            return this.$message.error(dt.meta.msg)
+          }
+          window.sessionStorage.setItem('token', dt.data.token)
+          // 成功，跳转到后台首页面
+          this.$router.push('/home')
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
@@ -43,6 +114,13 @@ export default {}
         border-radius: 50%;
         background-color: #eee;
       }
+    }
+    .el-form {
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      padding: 20px;
+      box-sizing: border-box;
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,11 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
 import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+// ESLint 规范要求，import需要在文件最前面设置
+Vue.use(ElementUI)
 // Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
+// axios配置请求公共根地址
+axios.defaults.baseURL = 'http://127.0.0.1:11333/api/private/v1/'
+// 给axios配置成为Vue的成员
+Vue.prototype.$http = axios
 new Vue({
   el: '#app',
   router,
