6a226a0615c05fd4418e3cb639477978747815eb
cdy03 <462180817@qq.com>
2019-04-25 00:03:24 +0800

    用户列表开发完成

--- src/components/user.vue
+++ src/components/user.vue
@@ -1,11 +1,107 @@
 <template>
-    <div>用户列表</div>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card class="box-card">
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input
+            placeholder="请输入搜索内容"
+            v-model="querycdt.query"
+            clearable
+            @clear="getUserList()"
+            @keyup.enter.native="getUserList()"
+          >
+            <el-button slot="append" icon="el-icon-search" @click="getUserList()"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="8">
+          <el-button type="primary">添加用户</el-button>
+        </el-col>
+      </el-row>
+      <el-table stripe :data="userList" border style="width: 100%">
+        <el-table-column type="index" label="序号" width="80"></el-table-column>
+        <el-table-column prop="username" label="用户名"></el-table-column>
+        <el-table-column prop="mobile" label="手机号码" width="180"></el-table-column>
+        <el-table-column prop="role_name" label="角色" width="180"></el-table-column>
+        <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
+        <el-table-column prop="mg_state" label="状态" width="80">
+          <el-switch v-model="info.row.mg_state" slot-scope="info"></el-switch>
+        </el-table-column>
+        <el-table-column label="操作" width="260">
+          <el-tooltip class="item" effect="dark" content="编辑" placement="top" :enterable="false">
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+          </el-tooltip>
+          <el-tooltip class="item" effect="dark" content="删除" placement="top" :enterable="false">
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+          </el-tooltip>
+          <el-tooltip class="item" effect="dark" content="分配角色" placement="top" :enterable="false">
+            <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+          </el-tooltip>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="querycdt.pagenum"
+        :page-sizes="[3,5,10,20]"
+        :page-size="querycdt.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="tot"
+      ></el-pagination>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
+  created() {
+    this.getUserList()
+  },
+  methods: {
+    handleSizeChange(val) {
+      this.querycdt.pagesize = val
+      this.getUserList()
+    },
+    handleCurrentChange(val) {
+      this.querycdt.pagenum = val
+      this.getUserList()
+    },
+    async getUserList() {
+      const { data: dt } = await this.$http.get('users', {
+        params: this.querycdt
+      })
+      // console.log(dt)
+
+      if (dt.meta.status !== 200) {
+        return this.$message.error(dt.meta.msg)
+      }
+      this.tot = dt.data.total
+      this.userList = dt.data.users
+    }
+  },
+  data() {
+    return {
+      //声明用户列表数据成员
+      tot: 0,
+      input: '',
+      userList: [],
+      querycdt: {
+        query: '', //被检索的模糊关键字
+        pagenum: 1, //当前页码设定【分页功能体现】
+        pagesize: 3 //设置每次获取3条记录[分页功能体现]
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.box-card {
+  margin-top: 15px;
+  box-shadow: 0 1px 1px rgba(0, 0, 10, 0.15);
+}
 </style>
